74b178970ff230b2d57c508030eef38dfc7a7c8b
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-09-06 18:40:39 +0300

    Generating URLs To Named Routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,18 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('login/facebook', 'Auth\AuthController@redirectToFacebook');
 Route::get('login/facebook/callback', 'Auth\AuthController@getFacebookCallback');
+
+//Route that responds to multiple HTTPS requests
+Route::match(['get', 'post'], '/dial', function () {
+    var_dump("AM A GET OR POST POST");
+});
+
+//Redirect Routes
+Route::redirect('/dials', '/home');
+
+//Name Routes
+Route::get('/testing/{name}', function ($name) {
+    echo "Route name is {$name}";
+})->name('profile');
+
+//Generating URLs To Named Routes
